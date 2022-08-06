13a7df87907abdb798c2bbe552bb2886659e33bc
gjxo12 <vhxpffltm@naver.com>
2022-08-06 19:56:33 +0900

    chore: checking path

--- main.py
+++ main.py
@@ -3,6 +3,34 @@ import sys
 import subprocess
 from pathlib import Path
 
+
+def move():
+    proc = subprocess.Popen('cd ', shell=True, stdin=subprocess.PIPE,
+                            stdout=subprocess.PIPE)
+    out, err = proc.communicate()
+    print(str(out.decode()))
+
+
+def movesst(argv: str):
+    print(f'{argv}  ddddd')
+    os.chdir(argv)
+    if Path(argv).is_dir():
+        print(f'{Path(argv)}  경로 있음')
+    move()
+
+
+def moveLib(argv: str):
+    print(f'{argv}  ddddd')
+    os.chdir(argv)
+    move()
+
+
+def moveEnter(argv: str):
+    print(f'{argv}  ddddd')
+    os.chdir(argv)
+    move()
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     file_path = sys.argv[2]
@@ -11,13 +39,13 @@ if __name__ == '__main__':
         sys.exit()
 
     cur_path = Path(__file__).parent.resolve()
-    print(cur_path)
     test = os.getcwd()
-    print(test)
-    os.chdir("..")
-    aPath = ""
-    bPath = ""
-    cPath = ""
-    proc = subprocess.Popen('cd & echo %cd% & echo a & echo b', shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-    out, err = proc.communicate()
-    print(str(out.decode()))
+    test = os.path.abspath("..")
+
+    moveLibsst(test + '\\' + sys.argv[1])
+    #moveLibanalysis(test + '\\' + sys.argv[2])
+    #moveEnterprise(test + '\\' + sys.argv[3])
+    print("Subprocess...........")
+    # proc = subprocess.Popen('cd & echo %cd% & echo a & echo b', shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    # out, err = proc.communicate()
+    # print(str(out.decode()))
